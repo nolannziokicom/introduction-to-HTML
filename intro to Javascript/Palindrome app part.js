737977036825552f77ba73883104867c1dd719ea
nolannziokicom <nolannzioki@gmail.com>
2026-06-24 20:55:39 +0300

    this is an app that tests if the word there is a palindrome or not

---
+++ intro to Javascript/Palindrome app part.js
@@ -0,0 +1,15 @@
+function palindrome(myString){
+    var input = myString.replace(/[^A-Z0-9]/ig,"").toLowerCase();
+
+    var reversedInput=input.split("").reverse().join("");
+
+    if (input == reversedInput){
+        document.write("<div>" + myString + " is a palindrome <div>")
+    }
+
+    else {
+        document.write("<div>" + myString + " is not a palindrome <div>")
+    }
+}
+
+palindrome ("racecar")
