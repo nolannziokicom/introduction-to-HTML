5fd9450cd7a665cdc17fd6a7feb131ede9436a07
nolannziokicom <nolannzioki@gmail.com>
2026-07-20 20:45:04 +0300

    This is showing my slider using js in html

---
+++ slider.js
@@ -0,0 +1,21 @@
+var index=0;
+function slider(){
+    var i;
+    var j= document.getElementsByClassName("myslides")
+
+    for (i=0; i<j.length; i++) {
+        j[i].style.display="none"
+    }
+
+    index++;
+    if (index>j.length) {
+        index=1;
+
+    }
+
+    x[index - 1].style.display="block";
+
+    setTimeout(slider,2000)
+}
+
+slider()
